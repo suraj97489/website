599c1b8616c0a27f67afe6f5966b8979dacbfe33
Suraj Atigre <suraj97489@gmail.com>
2022-02-09 18:07:06 +0530

    updated .env and firebaseproduction for enviroment and removed inject func

--- src/firebaseproduction.js
+++ src/firebaseproduction.js
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
+const prod = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
@@ -12,6 +12,18 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
+
+const dev = {
+  apiKey: process.env.REACT_APP_API_KEY_DEV,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN_DEV,
+  projectId: process.env.REACT_APP_PROJECT_ID_DEV,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET_DEV,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_DEV,
+  appId: process.env.REACT_APP_APP_ID_DEV,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID_DEV,
+};
+
+const firebaseConfig = process.env.REACT_APP_ENV === "prod" ? prod : dev;
 
 const firebaseApp = initializeApp(firebaseConfig);
 
