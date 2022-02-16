62990c9181df7421d034482272812f4fd1482aad
Suraj Atigre <suraj97489@gmail.com>
2022-02-16 14:09:51 +0530

    imported salon in store.js

--- src/App/store.js
+++ src/App/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import salon from "../features/salonSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    salon,
+  },
 });
