24a9b33d49a81bc9fe20cd0b439fd0512076f46b
Suraj Atigre <suraj97489@gmail.com>
2022-02-16 10:16:58 +0530

    title and description in Salons.js

--- src/AdminPanel/Salons/Salons.js
+++ src/AdminPanel/Salons/Salons.js
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
-import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebaseproduction";
 import "./Salons.css";
 import EachSalon from "./EachSalon";
 import { Redirect } from "react-router-dom";
 import UserContext from "../../context/UserContext";
+import { Helmet } from "react-helmet-async";
 
 function Salons() {
   const [salons, setSalons] = useState();
@@ -29,6 +30,15 @@ function Salons() {
   if (usercontext.customer?.email === process.env.REACT_APP_ADMIN_USERNAME) {
     return (
       <div className="Salons">
+        <Helmet>
+          <title>All Salons</title>
+          <meta
+            name="description"
+            content="All the salons list registered on the platform"
+          />
+          <link rel="canonical" href="/all-salons" />
+          <meta name="robots" content="noindex" />
+        </Helmet>
         <input className="Salons__input" placeholder="search salon..." />
         {salons?.map((salon, i) => (
           <EachSalon key={i} salon={salon} />
