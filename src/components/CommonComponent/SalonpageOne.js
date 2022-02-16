6021c1715269673f7a8581ac4bbc872da6694354
Suraj Atigre <suraj97489@gmail.com>
2022-02-16 14:16:45 +0530

    removed maincontext data from salonpageOne.js

--- src/components/CommonComponent/SalonpageOne.js
+++ src/components/CommonComponent/SalonpageOne.js
@@ -1,26 +1,26 @@
-import React, { useContext } from "react";
+import React from "react";
 import "./SalonpageOne.css";
-import Maincontext from "./../../context/MainContext";
+
+import { useSelector } from "react-redux";
 
 function SalonpageOne() {
-  const maincontext = useContext(Maincontext);
+  const salon = useSelector((state) => state.salon.salon);
 
   return (
     <>
       <div className="salonpage-container">
         <div className="salonpage-inner">
           <div className="salonpage-left">
-            <h2>{maincontext.salon?.salonName}</h2>
-            <p>{maincontext.salon?.address}</p>
-            <div className="mobilenumber">{maincontext.salon?.mobile} </div>
+            <h2>{salon?.salonName}</h2>
+            <p>{salon?.address}</p>
+            <div className="mobilenumber">{salon?.mobile} </div>
           </div>
 
           <div className="salonpage-right">
             <img
               className="salonPhoto"
               src={
-                maincontext.salon?.salonPhoto ||
-                process.env.REACT_APP_TEMPORARY_SALON_PIC
+                salon?.salonPhoto || process.env.REACT_APP_TEMPORARY_SALON_PIC
               }
               height="20rem"
               width="20rem"
@@ -34,7 +34,7 @@ function SalonpageOne() {
         <div className="salonpage-bottom">
           <small>website:</small>
           <p style={{ marginLeft: "2rem", fontSize: "1.5rem" }}>
-            {maincontext.salon?.website}
+            {salon?.website}
           </p>
         </div>
       </div>
