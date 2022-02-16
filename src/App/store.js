4b563e35d145863c43ed9f3662c143108c4f12b9
Suraj Atigre <suraj97489@gmail.com>
2022-02-16 18:43:54 +0530

    setup of mainSlice

--- src/App/store.js
+++ src/App/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import salon from "../features/salonSlice";
+import main from "../features/mainSlice";
 
 export const store = configureStore({
   reducer: {
     salon,
+    main,
   },
 });
