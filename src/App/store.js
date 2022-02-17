19ddda1050a6f31037f8eb01c9b7f6c60522b7aa
Suraj Atigre <suraj97489@gmail.com>
2022-02-17 14:54:39 +0530

    usercontext to userstate in store.js

--- src/App/store.js
+++ src/App/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import salon from "../features/salonSlice";
 import main from "../features/mainSlice";
-import usercontext from "../features/userSlice";
+import userstate from "../features/userSlice";
 
 export const store = configureStore({
   reducer: {
     salon,
     main,
-    usercontext,
+    userstate,
   },
 });
