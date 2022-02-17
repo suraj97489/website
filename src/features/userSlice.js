6deae00dde6f8e539a7adda81c7f40011eba624c
Suraj Atigre <suraj97489@gmail.com>
2022-02-17 14:30:58 +0530

    redux usercontext setUp

---
+++ src/features/userSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  customer: null,
+  userBooked: false,
+};
+
+export const userSlice = createSlice({
+  name: "usercontext",
+  initialState,
+  reducers: {
+    updateCustomer: (state, action) => {
+      state.customer = action.payload;
+    },
+    updateUserBooked: (state, action) => {
+      state.userBooked = action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { updateCustomer, updateUserBooked } = userSlice.actions;
+
+export default userSlice.reducer;
